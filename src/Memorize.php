1623fc0434cf28a28f9d8dc2caafb91557dc5f66
wiwofone <apeirion@gmail.com>
2014-07-12 15:24:23 +0200

    Sniff touches

--- src/Memorize.php
+++ src/Memorize.php
@@ -7,7 +7,7 @@
  * http://www.supermemo.com
  * http://www.supermemo.eu
  *
- * @copyright     Shahin Zarrabi 2014 (shahin@wiwo.se)
+ * @copyright 2014 Shahin Zarrabi (shahin@wiwo.se)
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -24,20 +24,21 @@ class Memorize
      * Calculates the interval (in days) in which to repeat the item after the
      * n:th repetition given the item's current E-factor.
      * 
-     * @param int $time How many times the item has been repeated
+     * @param int $time   How many times the item has been repeated
      * @param int $factor The item's current E-factor
+     *
      * @throws RangeException if the number of repetitions is lower than 1
      * @return float A ceiled value of the interval (in days)
      */
     public function calcInterval($time = 1, $factor = 2.5)
     {
-        if($time < 1) {
+        if ($time < 1) {
             throw new \RangeException('The number of repetitions must be 1 or higher');
         }
         
-        if($time == 1) {
+        if ($time == 1) {
             $interval = 1;
-        } elseif($time == 2) {
+        } elseif ($time == 2) {
             $interval = 6;
         } else {
             $interval = self::calcInterval($time - 1, $factor) * $factor;
@@ -53,13 +54,14 @@ class Memorize
      * the quality of the latest response to the item.
      * 
      * @param int $oldFactor The item's old E-factor
-     * @param int $quality The quality of the response to the item
+     * @param int $quality   The quality of the response to the item
+     *
      * @throws RangeException if the quality is not between 0 and 5
      * @return float The item's new E-factor
      */
     public function calcNewFactor($oldFactor = 2.5, $quality = 4)
     {
-        if($quality > 5 || $quality < 0) {
+        if ($quality > 5 || $quality < 0) {
             throw new \RangeException('Quality must be between 0 and 5');
         }
         
