a4ba566984ef92cc58d7cc5c29b99735887fc031
wiwofone <apeirion@gmail.com>
2014-08-15 01:24:57 +0200

    Documentation for CardQueue

--- src/CardQueue.php
+++ src/CardQueue.php
@@ -16,11 +16,29 @@ namespace Memorize;
 class CardQueue extends \SplPriorityQueue
 {
 
+    /**
+     * Reverse the default priority comparison to put cards due closer in time
+     * at the top.
+     * 
+     * @param int $priority1 Timestamp of the first node
+     * @param int $priority2 Timestamp of the second node
+     * 
+     * @return int Positive if bigger, 0 if equal or negative if smaller
+     */
     public function compare($priority1, $priority2)
     {
         return $priority1 - $priority2;
     }
     
+    /**
+     * Restrict the insert function to Card objects only, throwing an exception
+     * if this restriction is violated. If it is a Card, use the Card's nextTime
+     * as priority.
+     *
+     * @param Card $card
+     * @param int $priority
+     * @throws InvalidAgumentException when $card is not a Card
+     */
     public function insert($card, $priority) {
         if (get_class($card) == 'Memorize\Card') {
             parent::insert($card, $card->getNextTime());
