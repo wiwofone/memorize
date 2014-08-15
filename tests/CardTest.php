a2278f7d0938e00b2ec8803fffe262a7c7369082
wiwofone <apeirion@gmail.com>
2014-08-15 11:50:12 +0200

    Test new JSON and Array methods

--- tests/CardTest.php
+++ tests/CardTest.php
@@ -31,8 +31,7 @@ class CardTest extends \PHPUnit_Framework_TestCase
     /**
      * A repetition with a factor lower than 3 should reset the number of
      * repetitions, regardless of previous performance.
-     */
-     
+     */  
     public function testBadRepeat()
     {
         $card = new Card();
@@ -43,4 +42,57 @@ class CardTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(1, $card->getNumberOfRepeats());
     }
     
+    public function testFromJson()
+    {
+        $json = json_encode(array(
+            'question'        => 'This is the question',
+            'answer'          => 'This is the answer',
+            'numberOfRepeats' => '5',
+            'factor'          => '2.4',
+            'nextTime'        => '123'
+        ));
+        
+        $card = Card::fromJson($json);
+        
+        $this->assertEquals('This is the question', $card->getQuestion());
+        $this->assertEquals('This is the answer', $card->getAnswer());
+        $this->assertEquals(5, $card->getNumberOfRepeats());
+        $this->assertEquals(2.4, $card->getFactor());
+        $this->assertEquals(123, $card->getNextTime());
+    }
+    
+    public function testJsonSwitcharoo()
+    {
+        $card = new Card('What is your name?', 'Peter');
+        $json = $card->toJson();
+        $newCard = Card::fromJson($json);
+        $this->assertEquals('What is your name?', $newCard->getQuestion());
+    }
+    
+    public function testFromArray()
+    {
+        $array = array(
+            'question'        => 'This is the question',
+            'answer'          => 'This is the answer',
+            'numberOfRepeats' => '5',
+            'factor'          => '2.4',
+            'nextTime'        => '123'
+        );
+        
+        $card = Card::fromArray($array);
+        
+        $this->assertEquals('This is the question', $card->getQuestion());
+        $this->assertEquals('This is the answer', $card->getAnswer());
+        $this->assertEquals(5, $card->getNumberOfRepeats());
+        $this->assertEquals(2.4, $card->getFactor());
+        $this->assertEquals(123, $card->getNextTime());        
+    }
+    
+    public function testArraySwitcharoo()
+    {
+        $card = new Card('What is your name?', 'Peter');
+        $array = $card->toArray();
+        $newCard = Card::fromArray($array);
+        $this->assertEquals('What is your name?', $newCard->getQuestion());
+    }
 }
