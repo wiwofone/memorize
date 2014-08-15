77ef9675dc480eab36482864ea3a98c7395da682
wiwofone <apeirion@gmail.com>
2014-08-15 19:21:38 +0200

    Revert 4d930e1..8fe68d2
    This rolls back to commit 4d930e1612cb7518385530cfe6596bc698ff523a.

--- src/Repeater.php
+++ src/Repeater.php
@@ -9,14 +9,14 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+ 
 namespace Memorize;
-
+ 
 class Repeater
 {
 
     private $queue;
-
+    
     /**
      * Construct the repeater with an initial queue of cards.
      *
@@ -26,7 +26,7 @@ class Repeater
     {
         $this->queue = $queue;
     }
-
+    
     /**
      * Remove the current Card from the queue.
      */
@@ -34,7 +34,7 @@ class Repeater
     {
         return $this->queue->extract();
     }
-
+    
     /**
      * Insert a Card object in the queue.
      *
@@ -44,7 +44,7 @@ class Repeater
     {
         $this->queue->insert($card);
     }
-
+    
     /**
      * Get the current Card in the queue.
      */
@@ -52,7 +52,7 @@ class Repeater
     {
         return $this->queue->current();
     }
-
+    
     /**
      * Register an answer on a card and handle the card accordingly.
      *
@@ -63,15 +63,15 @@ class Repeater
     {
         /* Get the current card and call its repeat method */
         $this->getCard()->repeat($SM2, $quality);
-
+        
         /* Remove the card from the queue */
         $card = $this->removeCard();
-
+        
         /* If the quality was below 4, or the card is set to be repeated again
          * in less than a day, reinsert it to the queue. */
         if ($quality < 4 || $card->getNextTime() - time() < 60*60*24) {
             $this->reinsertCard($card);
         }
     }
-
+        
 }
